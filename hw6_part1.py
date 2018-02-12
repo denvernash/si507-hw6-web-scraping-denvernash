2cd2b96468c8dd3c2b94330de4b026a55ef78ba1
denvernash <35231540+denvernash@users.noreply.github.com>
2018-02-12 14:11:07 -0500

    finished part 1, partway through part 2

--- hw6_part1.py
+++ hw6_part1.py
@@ -7,3 +7,13 @@ print('\n*********** PART 1 ***********')
 print("Mark's page -- Alt tags\n")
 
 ### Your Part 1 solution goes here
+html = requests.get("http://newmantaylor.com/gallery.html").text
+
+soup = BeautifulSoup(html, 'html.parser')
+images = (soup.find_all('img'))
+
+for i in images:
+    if i.has_attr('alt'):
+        print(i["alt"])
+    else:
+        print("No alternative text provided!")
--- hw6_part2.py
+++ hw6_part2.py
@@ -8,3 +8,10 @@ print('\n*********** PART 2 ***********')
 print('Michigan Daily -- MOST READ\n')
 
 ### Your Part 2 solution goes here
+
+html = requests.get("https://www.michigandaily.com/").text
+
+soup = BeautifulSoup(html, 'html.parser')
+items = (soup.find('div', attrs = {'class': 'view view-most-read view-id-most_read view-display-id-panel_pane_1 view-dom-id-996'
+'58157999dd0ac5aa62c2b284dd266'}))
+print(items)
